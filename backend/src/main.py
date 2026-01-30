efd8575da909b0748c6f6f653788fdd78663520b
Rao0898 <raoahmedfarman@gmail.com>
2026-01-31 01:33:24 +0500

    Fix: main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -39,7 +39,8 @@ app = FastAPI(
 # Add CORS middleware - Allow all methods from various origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Frontend origins
+    allow_origins=["https://hackaton-2-todo-web.vercel.app",  # Tumhara Vercel URL
+        "http://localhost:3000"],  # Frontend origins
     allow_credentials=True,
     allow_methods=["*"],  # Allow all methods (GET, POST, PUT, DELETE, PATCH, OPTIONS, etc.)
     allow_headers=["*"],  # Allow all headers
